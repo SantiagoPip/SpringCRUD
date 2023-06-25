04cde49c330b147d68db116a9ee8fb691f5436d1
Santiago Moreno <santiago.moreno.dev01@gmail.com>
2023-06-25 17:28:01 -0500

    CRUD basico

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Santiago','Moreno','santiago@gmail.com','2023-02-20');
+INSERT INTO clientes(nombre,apellido,email,create_at) VALUES('Sergio','Moreno','sergio@gmail.com','2023-01-20');
